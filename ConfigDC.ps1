195d4235138da1f47ba839a94579b3b8ce1b2151
Kevin Remde <kevin@remde.net>
2016-02-17 08:39:07 -0600

    added GP edit and configure DC script

---
+++ ConfigDC.ps1
@@ -0,0 +1,23 @@
+﻿# 
+# Set group policy for Internet Explorer on member servers.
+# NOTE: This is not recommended for production servers, and only being done to allow servers in the lab
+# environment to be able to easily download files.
+#
+# The settings being enabled:
+# - Allow file downloads in the Internet Zone
+
+Import-Module grouppolicy
+
+# Get the GPO
+$gpo = Get-GPO -Name "Default Domain Policy" -Domain "contoso.com" 
+
+# Add the policy to allow WinRM
+$key = 'HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3'
+$params = @{
+    Key = $key;
+    ValueName = '1803';
+    Value = 0;
+    Type = 'Dword';
+}
+$gpo | Set-GPRegistryValue @params
+
