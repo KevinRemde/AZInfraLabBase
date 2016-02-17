5ef13786ab43e13f950dfb70305f881561e068b2
Kevin Remde <kevin@remde.net>
2016-02-17 07:42:34 -0600

    Enable IE Downloads

---
+++ EnableIEDownloads.ps1
@@ -0,0 +1,9 @@
+﻿#
+# This function enables file downloads in the Internet Zone on Internet Exporer in Windows Server.
+
+function Enable-IEDownloads {
+    $UserKey = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+    Set-ItemProperty -Path $UserKey -Name "1803" -Value 0
+    # Write-Host "IE downloads have been enabled." -ForegroundColor Green
+}
+Enable-IEDownloads
